6465d61a6eefc16dc4d66a1df2a46fe4a0d3660c
wjorgensen <wjorgensen29@gmail.com>
2024-04-05 02:06:16 -0400

    Almost finished homepage & added settings page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import styles from "@/styles/Home.module.scss";
 
-
 export default function Home() {
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [isZoomedIn, setIsZoomedIn] = useState(false);
+  const router = useRouter();
+
+  const handleMouseDown = () => {
+    setIsZoomed(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsZoomed(false);
+    setIsZoomedIn(true);
+  };
+
+  useEffect(() => {
+    if (isZoomedIn) {
+      const timer = setTimeout(() => {
+        router.push('/settings');
+      }, 3200); 
+
+      return () => clearTimeout(timer);
+    }
+  }, [isZoomedIn, router]);
+
   return (
-    <div className={styles.main}>
-      <img src="background.webp"></img>
-      <h1>RankRushd</h1>
+    <div className={`${styles.main} ${isZoomed ? styles.zoomed : ''} ${isZoomedIn ? styles.zoomedIn : ''}`}>
+      <div className={styles.backgroundImage}></div>
+      <div className={styles.content}>
+        <div>
+          <h1>Rank Rushd</h1>
+        </div>
+        <button
+          className={styles.play}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onTouchStart={handleMouseDown}
+          onTouchEnd={handleMouseUp}
+        >
+          PLAY
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Settings.module.scss"
+
+export default function Home() {
+    return (
+        <>
+            <p>Settings</p>
+        </>
+    );
+}
